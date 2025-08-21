0706b432877a7d2d693f35f3aaa0aa42d0310f52
Animesh <animeshyt68@gmail.com>
2025-08-21 10:27:15 +0530

    commit v1.4

---
+++ Shape/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new NewShape();
+        shape.draw(10,12);
+        shape.draw(10);
+    }
+}
---
+++ Shape/src/NewShape.java
@@ -0,0 +1,6 @@
+public class NewShape extends Shape{
+    public void draw(double l){
+        double area = l*l;
+        System.out.println("Area is "+" "+area);
+    }
+}
